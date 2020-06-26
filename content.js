de92b58e85458e4031142df1934260a722a05859
Frederick-S <mao_xiaodan@hotmail.com>
2020-06-26 17:54:31 +0800

    feature: remove external link confimation

--- content.js
+++ content.js
@@ -1,1 +1,7 @@
 document.querySelector('.TitleImage').remove()
+document.querySelectorAll('a.external')
+  .forEach(link => {
+    const href = link.getAttribute('href')
+
+    link.setAttribute('href', decodeURIComponent(href.split('target=')[1]))
+  })
